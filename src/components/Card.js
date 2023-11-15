525c33ecf5a1a465e587499438ce29304c49324b
Avinash Singh Ranawat <ranawat6@Avinashs-MacBook-Air.local>
2023-11-15 16:27:29 +0530

    final

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = (props) => {
     let review = props.review;
     return (
         <div className='flex flex-col md:relative'>
-          <div className='absolute top-[-7rem] z-[10] mx-auto'>
+          <div className='absolute top-[-7rem] z-[10] mx- auto'>
             <img className='aspect-square rounded-full w-[140 px] h-[140px] z-[25]' alt ="" 
             src = {review.image}/>
             <div className='w-[140px] h-[140px] bg-violet-500 rounded-full 
@@ -19,31 +19,19 @@ const Card = (props) => {
           </div>
 
           <div className='text-center mt-7'>
-            <p>{review.job}</p> 
+            <p className='text-violet-300 uppercase text-sm'>{review.job}</p> 
           </div>
-          <div>
+          <div className='text-voilet-400 mx-auto mt-5 '>
             <FaQuoteLeft/>
           </div>
-          <div>
+          <div className='text-center mt-4 text-slate-500'> 
             {review.text}
           </div>
-          <div>
+          <div className='text-voilet-400 mx-auto mt-5' >
             <FaQuoteRight/>
           </div>
-          <div>
-            <button>
-                +
-            </button>
-            <button>
-                * 
-            </button>
           </div>
-          <div>
-            <button>
-                Surprise me
-            </button>
-          </div>
-        </div>
+
     )
 }
 
